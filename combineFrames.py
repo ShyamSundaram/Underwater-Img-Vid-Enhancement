bc277bebc1addbf2216348ec44892e6c3ba3261a
vaibhave <vaibhavesvaibahve@gmail.com>
2021-10-12 19:14:44 +0530

    added fps fetch

--- combineFrames.py
+++ combineFrames.py
@@ -3,7 +3,9 @@ import os
 
 image_folder = 'frames'
 video_name = 'video.avi'
-
+#get frame rate
+file1 = open("fps.txt","r")
+fps = file1.read()
 def combineFrames(image_folder,video_name='video.avi'):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     for i in range(len(images)):
@@ -15,7 +17,7 @@ def combineFrames(image_folder,video_name='video.avi'):
 
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(video_name, 0, 29, (width,height))
+    video = cv2.VideoWriter(video_name, 0, fps, (width,height))
 
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, str(image))+'.png'))
--- getFrames.py
+++ getFrames.py
@@ -13,17 +13,17 @@ import cv2
 import time
 
 def getFrames(file):
-    vidcap=cv2.VideoCapture(file)
+    vidcap=cv2.VideoCapture(file) #capture and read the video file
     success,image=vidcap.read()
     c=0
     while success:
-        cv2.imwrite("frames/%d.png" % c, image)
+        cv2.imwrite("frames/%d.png" % c, image) #write the images into frames directory
         success,image=vidcap.read()
         c+=1
 
-# print("Started..")
-# start=time.time()
-# getFrames('video.mp4')
-# end=time.time()
 
-# print(f"Time taken is {end-start}")
+video = cv2.VideoCapture('video.mp4')
+#write fps in fps.txt
+fps = video.get(cv2.CAP_PROP_FPS)
+file1 = open("fps.txt","w")
+file1.write(str(int(fps)))
